b8d6e420d8901b8b0072156841f883ebf27503ac
katjam <katja@neontribe.co.uk>
2019-08-11 14:28:16 +0100

    Build against branch with matching keys

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ if [ -d contemplating-action ]; then
 fi
 
 #Grab the core files
-git clone https://$GIT_USER:$GIT_PASS@github.com/neontribe/contemplating-action.git --branch 1.0/release
+git clone https://$GIT_USER:$GIT_PASS@github.com/neontribe/contemplating-action.git --branch 1.0/hh-12-privacy-content-keys
+
 #Copy the ones we want to retain from this repo
 cp LICENSE contemplating-action/LICENSE
 cp README.md contemplating-action/README.md
