ef75d4450668ac9315309c2f4806642affff810f
Nikomus <nick.wade@live.co.uk>
2019-08-12 15:19:19 +0100

    Replace space

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ fi
 
 #Grab the core files
 git clone https://$GIT_USER:$GIT_PASS@github.com/neontribe/contemplating-action.git --branch 1.0/release
+
 #Copy the ones we want to retain from this repo
 cp LICENSE contemplating-action/LICENSE
 cp README.md contemplating-action/README.md
